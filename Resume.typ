f396d12899e63c412d0fdf9c9d54ff2cf05f8271
Nathan Fredericks <nathan@fredericks.dev>
2025-12-05 19:53:32 -0400

    Initial commit

---
+++ Resume.typ
@@ -0,0 +1,202 @@
+#let resume = yaml("resume.yaml")
+
+#set page(
+  margin: (x: 1.91cm, y: 1.91cm),
+)
+
+#set text(
+  font: "Helvetica",
+  size: 11pt,
+)
+
+#set par(justify: false, leading: 0.65em)
+
+#set list(marker: text(size: 16pt)[•], indent: 1em, spacing: 0.5em)
+
+#show link: it => underline(text(fill: rgb("#2A61BB"))[#it])
+
+#let contact(
+  name,
+  email1,
+  email2,
+  phone,
+  website,
+  codeberg,
+  linkedin
+) = {
+  grid(
+    columns: (1fr, auto),
+    align: (left + horizon, right + horizon),
+    column-gutter: 2em,
+    [
+      #text(size: 36pt, weight: "bold")[#name]
+    ],
+    [
+      #set align(right)
+      #link("mailto:" + email1)[#email1] \
+      #link("mailto:" + email2)[#email2] \
+      #phone \
+      #link(website)[Website] | #link(codeberg)[Codeberg] | #link(linkedin)[LinkedIn]
+    ]
+  )
+}
+
+
+#let section(title) = {
+  v(0.5em)
+  text(size: 14pt, weight: "bold")[#upper(title)]
+  v(-0.5em)
+}
+
+#let entry(title, date) = {
+  grid(
+    columns: (1fr, auto),
+    [*#title*],
+    [#date]
+  )
+}
+
+#let subentry(org) = {
+  v(-0.5em)
+  text[#org]
+  v(-0.25em)
+}
+
+#let project(title, date, codeberg: none, website: none) = {
+  entry(title, date)
+  v(-0.5em)
+  if website != none and codeberg != none {
+    link(website)[Website] + [ | ] + link(codeberg)[View on Codeberg]
+  } else if codeberg != none {
+    link(codeberg)[View on Codeberg]
+  } else if website != none {
+    link(website)[Website]
+  }
+}
+
+#let position(title, date, company, location) = {
+  entry(title, date)
+  subentry(company + ", " + location)
+}
+
+#contact(
+  resume.contact.name,
+  resume.contact.emails.at(0),
+  resume.contact.emails.at(1),
+  resume.contact.phone,
+  resume.contact.website,
+  resume.contact.codeberg,
+  resume.contact.linkedin
+)
+
+#section("Education")
+#for edu in resume.education {
+  entry(edu.degree, edu.dates)
+  subentry(edu.institution + ", " + edu.location)
+  
+  list(
+    [#underline[*Relevant Coursework:*] #edu.relevant_coursework.join(", ")],
+    [#underline[*Accolades:*] #edu.accolades.join(", ")]
+  )
+}
+
+#section("Skills")
+
+#grid(
+  columns: (1fr, 1fr),
+  column-gutter: 2em,
+  [
+    *KNOWN SOFTWARE* \
+    Software:
+    #for item in resume.skills.known_software.software [
+      - #item
+    ]
+
+    Frameworks:
+    #for item in resume.skills.known_software.frameworks [
+      - #item
+    ]
+  ],
+  [
+    *TECHNICAL* \
+    Proficient with:
+    #for item in resume.skills.technical.proficient_with [
+      - #item
+    ]
+
+    Familiar with:
+    #for item in resume.skills.technical.familiar_with [
+      - #item
+    ]
+  ]
+)
+
+#section("Work Experience")
+
+
+
+#for (i, job) in resume.work_experience.enumerate() {
+
+  position(job.title, job.dates, job.company, job.location)
+
+  
+
+  for item in job.description [
+
+    - #item
+
+  ]
+
+  
+
+  if i < resume.work_experience.len() - 1 {
+
+    v(0.5em)
+
+  }
+
+}
+
+
+
+#section("Projects")
+
+
+
+#for (i, proj) in resume.relevant_projects.enumerate() {
+
+  let website = if "links" in proj and "website" in proj.links { proj.links.website } else { none }
+
+  let codeberg = if "links" in proj and "codeberg" in proj.links { proj.links.codeberg } else { none }
+
+  
+
+  project(
+
+    proj.title,
+
+    proj.dates,
+
+    codeberg: codeberg,
+
+    website: website
+
+  )
+
+
+
+  for item in proj.description [
+
+    - #item
+
+  ]
+
+  
+
+  if i < resume.relevant_projects.len() - 1 {
+
+    v(0.5em)
+
+  }
+
+}
---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Resume struct {
+	Contact struct {
+		Name     string   `yaml:"name"`
+		Emails   []string `yaml:"emails"`
+		Phone    string   `yaml:"phone"`
+		Website  string   `yaml:"website"`
+		Codeberg string   `yaml:"codeberg"`
+		Linkedin string   `yaml:"linkedin"`
+	} `yaml:"contact"`
+	Education []struct {
+		Degree             string   `yaml:"degree"`
+		Institution        string   `yaml:"institution"`
+		Location           string   `yaml:"location"`
+		Dates              string   `yaml:"dates"`
+		RelevantCoursework []string `yaml:"relevant_coursework"`
+		Accolades          []string `yaml:"accolades"`
+	} `yaml:"education"`
+	Skills struct {
+		KnownSoftware struct {
+			Software   []string `yaml:"software"`
+			Frameworks []string `yaml:"frameworks"`
+		} `yaml:"known_software"`
+		Technical struct {
+			ProficientWith []string `yaml:"proficient_with"`
+			FamiliarWith   []string `yaml:"familiar_with"`
+		} `yaml:"technical"`
+	} `yaml:"skills"`
+	RelevantProjects []struct {
+		Title string `yaml:"title"`
+		Dates string `yaml:"dates"`
+		Links struct {
+			Codeberg string `yaml:"codeberg"`
+			Website  string `yaml:"website"`
+		} `yaml:"links"`
+		Description []string `yaml:"description"`
+	} `yaml:"relevant_projects"`
+	WorkExperience []struct {
+		Title       string   `yaml:"title"`
+		Company     string   `yaml:"company"`
+		Location    string   `yaml:"location"`
+		Dates       string   `yaml:"dates"`
+		Description []string `yaml:"description"`
+	} `yaml:"work_experience"`
+}
+
+func main() {
+	data, err := os.ReadFile("resume.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	var r Resume
+	if err := yaml.Unmarshal(data, &r); err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create("resume.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	w := func(format string, a ...interface{}) {
+		fmt.Fprintf(f, format, a...)
+	}
+
+	w("%s\n", r.Contact.Name)
+	for _, email := range r.Contact.Emails {
+		w("%s\n", email)
+	}
+	w("%s\n", r.Contact.Phone)
+	w("Website (%s), Codeberg (%s), LinkedIn (%s)\n\n\n", r.Contact.Website, r.Contact.Codeberg, r.Contact.Linkedin)
+
+	w("EDUCATION\n")
+	for _, edu := range r.Education {
+		w("%s | %s\n", edu.Degree, edu.Dates)
+		w("%s, %s\n", edu.Institution, edu.Location)
+		if len(edu.RelevantCoursework) > 0 {
+			w("- Relevant Coursework: %s\n", strings.Join(edu.RelevantCoursework, ", "))
+		}
+		if len(edu.Accolades) > 0 {
+			w("- Accolades: %s\n", strings.Join(edu.Accolades, ", "))
+		}
+		w("\n\n")
+	}
+
+	w("SKILLS\n")
+	w("KNOWN SOFTWARE\n")
+	if len(r.Skills.KnownSoftware.Software) > 0 {
+		w("Software:\n")
+		for _, item := range r.Skills.KnownSoftware.Software {
+			w("  - %s\n", item)
+		}
+		w("\n")
+	}
+	if len(r.Skills.KnownSoftware.Frameworks) > 0 {
+		w("Frameworks:\n")
+		for _, item := range r.Skills.KnownSoftware.Frameworks {
+			w("  - %s\n", item)
+		}
+		w("\n")
+	}
+
+	w("TECHNICAL\n")
+	if len(r.Skills.Technical.ProficientWith) > 0 {
+		w("Proficient with:\n")
+		for _, item := range r.Skills.Technical.ProficientWith {
+			w("  - %s\n", item)
+		}
+		w("\n")
+	}
+	if len(r.Skills.Technical.FamiliarWith) > 0 {
+		w("Familiar with:\n")
+		for _, item := range r.Skills.Technical.FamiliarWith {
+			w("  - %s\n", item)
+		}
+		w("\n")
+	}
+	w("\n")
+
+	w("WORK EXPERIENCE\n")
+	for _, work := range r.WorkExperience {
+		w("%s | %s\n", work.Title, work.Dates)
+		w("%s, %s\n", work.Company, work.Location)
+		for _, desc := range work.Description {
+			w("- %s\n", desc)
+		}
+		w("\n")
+	}
+	w("\n")
+
+	w("PROJECTS\n")
+	for _, proj := range r.RelevantProjects {
+		w("%s | %s\n", proj.Title, proj.Dates)
+
+		var links []string
+		if proj.Links.Website != "" {
+			links = append(links, fmt.Sprintf("Website (%s)", proj.Links.Website))
+		}
+		if proj.Links.Codeberg != "" {
+			links = append(links, fmt.Sprintf("View on Codeberg (%s)", proj.Links.Codeberg))
+		}
+		if len(links) > 0 {
+			w("%s\n", strings.Join(links, " | "))
+		}
+
+		for _, desc := range proj.Description {
+			w("- %s\n", desc)
+		}
+		w("\n")
+	}
+	w("\n")
+}
